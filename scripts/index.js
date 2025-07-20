77de5d3bbdc37c947d47b04e7866098eca07b5ab
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2025-07-20 10:27:38 -0500

    Updated Modal Close Button

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(".modal__close-button");
+
+
+editProfileButton.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened")
+})
+
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened")
+})
