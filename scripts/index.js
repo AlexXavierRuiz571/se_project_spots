9e0906ad6a5315821015362df592615fb1fda35b
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2025-07-20 10:21:12 -0500

    Implemented Javascript to project

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseButton = editProfileModal.querySelector(".modal__close-button");
