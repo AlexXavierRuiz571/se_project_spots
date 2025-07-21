61d749dc586402ecafaa2cdf4627baa50d5bce0a
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2025-07-21 17:51:28 -0500

    Updated Edit Profile Form Fields

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,36 @@
+//-----------Profile Modal------------
+
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
 
 editProfileButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent
+  editProfileDescriptionInput.value = profileDescriptionElement.textContent
   editProfileModal.classList.add("modal_is-opened");
 });
 editProfileCloseButton.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
+//-----------Post Modal------------
+
 const newPostButton = document.querySelector(".profile__post-button");
 const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseButton = newPostModal.querySelector(
-  ".modal__close-button"
-);
+const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
 
 newPostButton.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
