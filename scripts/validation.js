a2886d6d907d4edbce5dee33fadcd3ece9cf7b82
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2025-08-09 09:11:26 -0500

    fix validation to clear errors

--- scripts/validation.js
+++ scripts/validation.js
@@ -12,12 +12,16 @@ function updateErrorForInput(formElement, inputElement, settings) {
 
   if (!inputElement.validity.valid) {
     inputElement.classList.add(settings.inputErrorClass);
-    errorElement.textContent = inputElement.validationMessage;
-    errorElement.classList.add(settings.errorClass);
+    if (errorElement) {
+      errorElement.textContent = inputElement.validationMessage;
+      errorElement.classList.add(settings.errorClass);
+    }
   } else {
     inputElement.classList.remove(settings.inputErrorClass);
-    errorElement.textContent = "";
-    errorElement.classList.remove(settings.errorClass);
+    if (errorElement) {
+      errorElement.textContent = "";
+      errorElement.classList.remove(settings.errorClass);
+    }
   }
 }
 
@@ -54,20 +58,39 @@ function enableValidation(settings) {
 }
 
 function clearValidation(formElement, settings) {
-  const inputList = Array.from(
-    formElement.querySelectorAll(settings.inputSelector)
-  );
-  const buttonElement = formElement.querySelector(
-    settings.submitButtonSelector
-  );
+  const inputs = Array.from(formElement.querySelectorAll(settings.inputSelector));
+  const button = formElement.querySelector(settings.submitButtonSelector);
 
-  inputList.forEach((inputElement) =>
-    updateErrorForInput(formElement, inputElement, settings)
-  );
+  inputs.forEach((input) => {
+    input.classList.remove(settings.inputErrorClass);
 
-  const hasInvalid = inputList.some((input) => !input.validity.valid);
-  buttonElement.disabled = hasInvalid;
-  buttonElement.classList.toggle(settings.inactiveButtonClass, hasInvalid);
+    const errorEl = formElement.querySelector(`#${input.id}-error`);
+    if (errorEl) {
+      errorEl.textContent = "";
+      errorEl.classList.remove(settings.errorClass);
+    }
+  });
+
+  if (button) {
+    button.disabled = true;
+    button.classList.add(settings.inactiveButtonClass);
+  }
 }
+
+newPostForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  closeModal(newPostModal);
+  newPostForm.reset();
+  clearValidation(newPostForm, validationSettings);
+});
+
+editProfileForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  closeModal(editProfileModal);
+  editProfileForm.reset();
+  clearValidation(editProfileForm, validationSettings);
+});
+
 
 enableValidation(validationSettings);
